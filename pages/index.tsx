7eca8046eed87730031e2c5c5ec6ab2c5114761c
j.dsouza <j.dsouza@freetimecompany.nl>
2023-02-17 17:33:41 +0530

    sentry io

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
 	return (
-		<h1>SENTRY IO DEMO</h1>
-	)
+		<>
+			<h1>SENTRY IO DEMO {process.env.NEXT_PUBLIC_ENV}</h1>
+			<button
+				type="button"
+				onClick={() => {
+					throw new Error(
+						"Sentry Frontend Error " + process.env.NEXT_PUBLIC_ENV
+					);
+				}}
+			>
+				Throw error
+			</button>
+		</>
+	);
 }
---
+++ sentry.edge.config.js
@@ -0,0 +1,21 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever middleware or an Edge route handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+	dsn: SENTRY_DSN || 'https://6c5cbd20d36f4d2bab4d452a66a364d9@o4504683684888576.ingest.sentry.io/4504683697668096',
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1.0,
+	environment: process.env.NEXT_PUBLIC_ENV || 'development',
+
+	// ...
+	// Note: if you want to override the automatic release value, do not set a
+	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
+	// that it will also get attached to your source maps
+});
