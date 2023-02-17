bde6979da9b36cc5e9910624957628af984d6ee9
j.dsouza <j.dsouza@freetimecompany.nl>
2023-02-17 18:22:44 +0530

    about us

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,13 @@
 export default function About() {
+	const clickMe = () => {
+		var x: any;
+		x.test = 1;
+	};
+
 	return (
 		<>
 			<h1>SENTRY IO DEMO {process.env.NEXT_PUBLIC_ENV}</h1>
-			<button
-				type="button"
-				onClick={() => {
-					throw new Error(
-						"Sentry Frontend Error on About us page " +
-							process.env.NEXT_PUBLIC_ENV
-					);
-				}}
-			>
+			<button type="button" onClick={clickMe}>
 				Throw error
 			</button>
 		</>
